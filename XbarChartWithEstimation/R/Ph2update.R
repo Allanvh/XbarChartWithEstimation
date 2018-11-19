f6db6219ce386cc65562462ead8aed6071c694f0
FelipeSJardim <felipe_s_jardim@hotmail.com>
2018-11-19 14:32:20 -0600

    Ph2 update

---
+++ XbarChartWithEstimation/R/Ph2update.R
@@ -0,0 +1,18 @@
+check.packages <- function(pkg){
+    new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+    if (length(new.pkg)) 
+        install.packages(new.pkg, dependencies = TRUE)
+    sapply(pkg, require, character.only = TRUE)
+}
+
+
+###########################################################################################
+
+packages<-c("mvtnorm")
+check.packages(packages)
+
+###########################################################################################
+
+require(mvtnorm)
+
+source(file = 'https://raw.githubusercontent.com/bolus123/PH1AND2XBAR/master/R/main.R')
